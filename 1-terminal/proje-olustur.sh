24cf2424fcd5d47ed0bd3062f50f55c97bcc8aa7
Samet <abdulsametkucuk01@gmail.com>
2026-03-07 04:05:25 +0300

    1. adım: terminal ve bash scriptleri tamamlandı

---
+++ 1-terminal/proje-olustur.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Proje Oluşturuluyor..."
+
+mkdir -p yeni-proje1/src
+mkdir -p yeni-proje1/docs
+
+echo "# Benim Projem" > yeni-proje/docs/README.md
+echo "print('Merhaba Dünya')" > yeni-proje/src/uygulama.py
+
+echo "Hazır! Klasörler ve dosyalar oluşturuldu."
+
